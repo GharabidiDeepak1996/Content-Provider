7c64feadf981c31e000eba2f2cd441541a55070c
Deepak <dgharabidi@gmail.com>
2019-09-26 01:25:12 +0530

    first

---
+++ Content Provider/ContentProviderDemo-master/ContentProviderDemo-master/README.md
@@ -0,0 +1,11 @@
+# Content Provider Demo
+
+> A basic sample which shows how to use content provider in Android to share data between two apps.
+
+- AppOne used to add data.
+- AppTwo reads the data using shared content provider.
+- Implemented [Swipe Refresh Layout](https://developer.android.com/training/swipe/add-swipe-interface.html).
+
+### Author
+
+- Gokul Nath KP
---
+++ Content Provider/ContentProviderDemo-master/ContentProviderDemo-master/app-one/src/main/java/com/kpgn/appone/MainActivity.java
@@ -0,0 +1,42 @@
+package com.kpgn.appone;
+
+import android.content.ContentValues;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import com.kpgn.common.ApplicationConstant;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @BindView(R.id.et_email_id)
+    EditText mEmailID;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick(R.id.b_add)
+    public void actionAdd(View view) {
+        ContentValues values = new ContentValues();
+        values.put(ApplicationConstant.COLUMN_EMAIL, mEmailID.getText().toString());
+        Log.d( TAG, "actionAdd: " +values);
+
+        Uri uri = getContentResolver().insert(CustomContentProvider.CONTENT_URI,values);
+        Log.d( TAG, "actionAdd: "+uri );
+        mEmailID.setText("");
+        Toast.makeText(getBaseContext(), "Email Added Successfully!", Toast.LENGTH_LONG).show();
+    }
+}
